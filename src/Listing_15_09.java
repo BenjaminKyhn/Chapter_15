8c86606738acb6c7bd09cd3aab7208a5a247ec90
Benjamin Kyhn <benkyhn@hotmail.com>
2020-04-06 17:33:40 +0200

    Added listing 15.9

--- src/Listing_15_09.java
+++ src/Listing_15_09.java
@@ -1,2 +1,56 @@
-public class Listing_15_09 {
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseButton;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+public class Listing_15_09 extends Application {
+    private CirclePane circlePane = new CirclePane();
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        HBox hBox = new HBox();
+        hBox.setSpacing(10);
+        hBox.setAlignment(Pos.CENTER);
+        Button btEnlarge = new Button("Enlarge");
+        Button btShrink = new Button("Shrink");
+        hBox.getChildren().add(btEnlarge);
+        hBox.getChildren().add(btShrink);
+
+        btEnlarge.setOnAction(e -> circlePane.enlarge());
+        btShrink.setOnAction(e -> circlePane.shrink());
+
+        BorderPane borderPane = new BorderPane();
+        borderPane.setCenter(circlePane);
+        borderPane.setBottom(hBox);
+        BorderPane.setAlignment(hBox, Pos.CENTER);
+
+        Scene scene = new Scene(borderPane, 200, 150);
+        stage.setTitle("ControlCircle");
+        stage.setScene(scene);
+        stage.show();
+
+        circlePane.setOnMouseClicked(e -> {
+            if (e.getButton() == MouseButton.PRIMARY) {
+                circlePane.enlarge();
+            }
+            if (e.getButton() == MouseButton.SECONDARY) {
+                circlePane.shrink();
+            }
+        });
+
+        scene.setOnKeyPressed(e -> {
+            if (e.getCode() == KeyCode.UP) {
+                circlePane.enlarge();
+            }
+            else if (e.getCode() == KeyCode.DOWN) {
+                circlePane.shrink();
+            }
+        });
+    }
 }
+
