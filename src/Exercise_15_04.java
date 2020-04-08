ca4aaf478673b1b4f81285f6df7ccf399bd2aaf2
Benjamin Kyhn <benkyhn@hotmail.com>
2020-04-08 10:50:45 +0200

    Added exercise 15.4

--- src/Exercise_15_04.java
+++ src/Exercise_15_04.java
@@ -1,64 +1,87 @@
 import javafx.application.Application;
-import javafx.geometry.HPos;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Exercise_15_04 extends Application {
-    private TextField tfInvestmentAmount = new TextField();
-    private TextField tfNumberOfYears = new TextField();
-    private TextField tfAnnualInterestRate = new TextField();
-    private TextField tfFutureValue = new TextField();
-    private Button btCalculate = new Button("Calculate");
+    private Label labelN1 = new Label("Number 1:");
+    private Label labelN2 = new Label("Number 2:");
+    private Label labelResult = new Label("Result:");
+    private TextField tfN1 = new TextField();
+    private TextField tfN2 = new TextField();
+    private TextField tfResult = new TextField();
+    private Button btAdd = new Button("Add");
+    private Button btSubtract = new Button("Subtract");
+    private Button btMultiply = new Button("Multiply");
+    private Button btDivide = new Button("Divide");
 
     @Override
     public void start(Stage stage) {
-        // Create UI
-        GridPane gridPane = new GridPane();
-        gridPane.setHgap(4);
-        gridPane.setVgap(4);
-        gridPane.add(new Label("Investment Amount:"), 0, 0);
-        gridPane.add(tfInvestmentAmount, 1, 0);
-        gridPane.add(new Label("Number of Years:"), 0, 1);
-        gridPane.add(tfNumberOfYears, 1, 1);
-        gridPane.add(new Label("Annual Interest Rate:"), 0, 2);
-        gridPane.add(tfAnnualInterestRate, 1, 2);
-        gridPane.add(new Label("Future Value:"), 0, 3);
-        gridPane.add(tfFutureValue, 1, 3);
-        gridPane.add(btCalculate, 1, 5);
+        /** Should have used a GridPane instead as it would have looked nicer */
+        HBox hBox1 = new HBox();
+        HBox hBox2 = new HBox();
+        VBox vBox = new VBox();
 
-        // Set properties for UI
-        gridPane.setAlignment(Pos.CENTER);
-        tfInvestmentAmount.setAlignment(Pos.BOTTOM_RIGHT);
-        tfNumberOfYears.setAlignment(Pos.BOTTOM_RIGHT);
-        tfAnnualInterestRate.setAlignment(Pos.BOTTOM_RIGHT);
-        tfFutureValue.setAlignment(Pos.BOTTOM_RIGHT);
-        tfFutureValue.setEditable(false);
-        GridPane.setHalignment(btCalculate, HPos.RIGHT);
+        hBox1.getChildren().addAll(labelN1, tfN1, labelN2, tfN2, labelResult, tfResult);
+        hBox2.getChildren().addAll(btAdd, btSubtract, btMultiply, btDivide);
+        vBox.getChildren().addAll(hBox1, hBox2);
 
-        // Process events
-        btCalculate.setOnAction(e -> calculateFutureValue());
+        hBox1.setAlignment(Pos.CENTER);
+        hBox2.setAlignment(Pos.CENTER);
+        tfN1.setMaxWidth(50);
+        tfN2.setMaxWidth(50);
+        tfResult.setMaxWidth(50);
+        tfResult.setEditable(false);
 
-        // Create a scene and place it in the stage
-        Scene scene = new Scene(gridPane, 300, 200);
-        stage.setTitle("Future Value Calculator");
+        btAdd.setOnAction(e -> add());
+        btSubtract.setOnAction(e -> subtract());
+        btMultiply.setOnAction(e -> multiply());
+        btDivide.setOnAction(e -> divide());
+
+        Scene scene = new Scene(vBox, 300, 200);
+        stage.setTitle("Calculator");
         stage.setScene(scene);
         stage.show();
     }
 
-    private void calculateFutureValue() {
-        // Get values from text fields
-        double investment = Double.parseDouble(tfInvestmentAmount.getText());
-        int year = Integer.parseInt(tfNumberOfYears.getText());
-        double interest = Double.parseDouble(tfAnnualInterestRate.getText());
+    private void add() {
+        double n1 = Double.parseDouble(tfN1.getText());
+        double n2 = Double.parseDouble(tfN2.getText());
 
-        double futureValue = investment * Math.pow((1 + (interest / 100)), year);
+        double result = n1 + n2;
 
-        // Display monthly payment and total payment
-        tfFutureValue.setText(String.format("$%.2f", futureValue));
+        tfResult.setText(String.format("%.2f", result));
+    }
+
+    private void subtract() {
+        double n1 = Double.parseDouble(tfN1.getText());
+        double n2 = Double.parseDouble(tfN2.getText());
+
+        double result = n1 - n2;
+
+        tfResult.setText(String.format("%.2f", result));
+    }
+
+    private void multiply() {
+        double n1 = Double.parseDouble(tfN1.getText());
+        double n2 = Double.parseDouble(tfN2.getText());
+
+        double result = n1 * n2;
+
+        tfResult.setText(String.format("%.2f", result));
+    }
+
+    private void divide() {
+        double n1 = Double.parseDouble(tfN1.getText());
+        double n2 = Double.parseDouble(tfN2.getText());
+
+        double result = n1 / n2;
+
+        tfResult.setText(String.format("%.2f", result));
     }
 }
